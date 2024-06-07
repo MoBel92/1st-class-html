b8cf7368df112e88ecbc0e2b938734ac8c3b9623
MoBel92 <belhadj.mohamad@gmail.com>
2024-06-07 15:24:31 +0100

    javascript file added

---
+++ script.js
@@ -0,0 +1,28 @@
+
+show_image(index);
+
+
+function show_image(){
+index += i ;
+
+let images = document.getElementsByClassName("image");
+let dots = document.getElementsByClassName("dot");
+
+for(let i=0 ; i< images.length;i++){
+    images[i].style.display = "none";
+
+    for(let i=0; i<dots.length; i++){
+
+        dots[i].className = dots[i].className.replace(" active ", "")
+    }
+
+if(index > images.length - 1 ){
+    index =0;
+}
+if (index < 0 ){
+    index = images.length - 1;
+}
+
+index[index].style.display ="block";
+dots[index].className += "active";
+}}
